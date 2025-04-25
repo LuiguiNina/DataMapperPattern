05fbdbae9f14ceb74ed6d955e3d8e421a594a6a2
LuiguiNV <ln2019065166@virtual.upt.pe>
2025-04-25 15:38:05 -0500

    .

--- data-mapper-pattern-python/domain/customer.py
+++
@@ -1,8 +0,0 @@
-class Customer:
-    def __init__(self, id=None, name="", email=""):
-        self.id = id
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return f"<Customer id={self.id}, name='{self.name}', email='{self.email}'>"
--- data-mapper-pattern-python/tests/test_customer.py
+++
@@ -1,5 +0,0 @@
-from domain.customer import Customer
-
-def test_customer_repr():
-    customer = Customer(id=1, name="Bob", email="bob@example.com")
-    assert repr(customer) == "<Customer id=1, name='Bob', email='bob@example.com'>"
